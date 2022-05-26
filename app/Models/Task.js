c49f3e465c9c3061b6ce6440880904140804aae3
Dane Barber <dane.barber@me.com>
2022-05-26 16:47:03 -0600

    tasks

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -3,7 +3,19 @@ import { generateId } from "../Utils/generateId.js"
 export class Task {
   constructor(data) {
     this.id = generateId()
+    this.listId = data.listId
     this.title = data.title
+    this.checked = data.checked
     this.color = data.color
   }
+
+  get template() {
+    return `
+    <p class="d-flex justify-content-between" style="${this.color}"> <span> ${this.title}</span><span></span>
+    <i class="mdi mdi-delete selectable px-3" onclick="app.itemsController.remove('${this.id}')"></i>
+    </p>
+    `
+  }
+
+
 }
